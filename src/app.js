06a69f8240adc70a5357c835223fc35f1fd21197
grimreaperLinux <ani9431619703@gmail.com>
2021-09-14 17:19:06 +0530

    Weather App files

---
+++ src/app.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const path = require('path')
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
+const app = express()
+const hbs = require('hbs')
+
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+//setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+//setup handlebars and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.get('', (req, res) => {
+
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Aniket Raj'
+    })
+})
+
+app.get('/weather', (req, res) => {
+
+    if (!req.query.address) {
+        return res.send({
+            error: "You done fucked up bruh"
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error)
+            return res.send({ error: error })
+
+        forecast(latitude, longitude, (error, foreCastdata) => {
+            if (error)
+                return res.send({ error: error })
+            res.send({
+                location: location,
+                forecast: foreCastdata
+            })
+        })
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Aniket Raj'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text',
+        title: 'Help'
+    })
+})
+
+app.get('/help/*', (req, res) => { // '*' indicates that everything else is a match
+    res.render('404', {
+        title: '404',
+        name: 'Aniket',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => { // '*' indicates that everything else is a match
+    res.render('404', {
+        title: '404',
+        name: 'Aniket',
+        errorMessage: '404 not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up and running')
+})
+
+
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+  const url = 'http://api.weatherstack.com/current?access_key=3e36d08c3646b9314b196098baf7e0b8&query=' + latitude + ',' + longitude + ''
+  request({ url: url, json: true }, (error, {body}) => {
+    if (error)
+      callback('Unable to connect to the INTERNET', undefined)
+    else if (body.error) {
+      callback('Unable to find location', undefined)
+    }
+    else {
+      callback(undefined, 
+        "The temperature of the required place is: " +body.current.temperature+ "C"
+      )
+    }
+  })
+}
+
+module.exports = forecast
